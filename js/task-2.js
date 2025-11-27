4780c15aa4eb146c3541cd01cc37ed2f2373fb74
veronika <sustv153@gmail.com>
2025-11-27 18:58:58 +0200

    done -+

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,23 @@
+
+class Storage {
+    #items;
+    
+    constructor(items) {
+        this.#items = items;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items = newItem;
+    }
+    
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(item => item !== itemToRemove);
+    }
+}
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
